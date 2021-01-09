d05cfcd281965825c296995e51a35975a1f8dc1b
Michael Clinesmith <clinmic2@bvu.edu>
2021-01-09 00:21:03 -0600

    This needs so much refactoring

--- intents/intents.py
+++ intents/intents.py
@@ -30,6 +30,8 @@ class Intent():
             return "Digit found. Please spell out numbers, instead " + \
                         "(i.e. 'five' rather than '5')."
 
+        # Creating a variable to help with tracking curly brackets
+        openCurlyIndex = -1
         for i in range(0, len(phrase), 1):
             # Periods should only be used to denote the single letters of initials
             # Amazons example is n. b. a. Any other periods aren't allowed
@@ -39,17 +41,35 @@ class Intent():
                             "used to seperate initials (e.g. 'n. b. a.')."
 
             # Single ticks should only be used for possives or conjunctions
-            if phrase[i] == "'":
+            elif phrase[i] == "'":
                 if not phrase[i-1].isalpha() and not phrase[i+1].isalpha():
                     return "The \"'\" character can only be used for " + \
                             "contractions and possessives " + \
                             "(e.g. shouldn't or Timmy's)."
 
             # Hyphens should only be used to join two words 
-            if phrase[i] == "-":
+            elif phrase[i] == "-":
                 if not phrase[i-1].isalpha() and not phrase[i+1].isalpha():
                     return "'-' should only be used to join hyphenated words " + \
                             "(e.g. editor-in-chief)"
+
+            # Curly brackets must open and close before another set of curly
+            # brackets starts
+            elif phrase[i] == "}":
+                if openCurlyBracket != -1:
+                    return "Closing bracket with no opening bracket"
+                # Resetting openCurlyBrace if this does close an opening
+                # curly brace
+                else:
+                    openCurlyBrace == -1
+            elif phrase[i] == "{":
+                if openCurlyBrace == -1:
+                    return "Multiple opening brackets with no closing brackets"
+                else:
+                    openCurlyBrace = i
+            elif i == len(phrase)-1:
+                if openCurlyBrace != -1:
+                    return "Opening bracket with no closing bracket" 
 
         return ""
 
@@ -60,7 +80,29 @@ class Intent():
 class Utterance(Intent):
     def __init__(self, phrase):
         self.phrase = phrase
-        self.slots = []
+        self.slots = self.grabslots()
+
+    def grabSlots(self):
+        slots = []
+        for i in range(0, len(self.phrase), 1):
+            if self.phrase[i] == "{":
+                beginSlot = i+1
+                while self.phrase[i] != "}":
+                    i += i
+                endSlot = i
+                newSlotName = self.phrase[beginSlot:endSlot]
+                newSlot = Slot(newSlotName)
+                slots.append(newSlot)
+
+        return slots
+
+    def replaceWord(self, word, slotName):
+        substitute = "{" + slotName + "}"
+        self.phrase.replace(word, substitute)
+        self.slots.append(slotName)
+
+    def __repr__(self):
+        return self.phrase
 
 class Slot(Utterance):
     def __init__(self, slotName):
--- interface/interface.py
+++ interface/interface.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from sys import argv
-from intents.addIntent import Intent, Utterance, Slot
+from intents.intents import Intent, Utterance, Slot
 
 class Interface():
     def __init__(self):
@@ -47,28 +47,44 @@ class Interface():
         # this variable to be printed after the screen is cleared.
         self.output = "The name of your intent is '{}'. ".format(intentName) + \
                 "Change it at any time by typing 'change intent name'."
-
-        self.intent = AddIntent(intentName)
+        #TODO: Refactor this so that it actually kinda makes sense. THen test
+        self.intent = Intent(intentName)
         self.prompt = "Please start by adding at least one sample utterance " + \
             "(type 'rules' to learn more about how to write an utterance):\n"
+        self.promptMode = "addUtterance"
 
-        promptMode = "utterance"
-        while promptMode != "quit":
+        while self.promptMode != "quit":
             subprocess.run("clear")
             print(self.output)
             command = input(self.prompt)
 
-            if promptMode == "utterance":
+            if self.promptMode == "addUtterance":
                 self.addUtterances(command)
-            if promptMode == "slot":
+            elif self.promptMode == "chooseUtterance":
+                if command == "q":
+                    pass #FIXME: Skip past slots
+                else:
+                    self.chooseUtterance(command)
+            elif self.promptMode == "slot":
+                self.prepCurrUterance()
                 self.addSlots(command)
+
+    
+
+    def setIntentsPrompts(self, mode):
+        if mode == "addUtterance":
+            self.prompt = \
+            "Enter another utterance or enter 'q' to start creating slots:\n"
+        elif mode == "chooseUtterance":
+            self.prompt = "Please enter an utterance number: "
+            
 
     def addUtterances(self, command):
         # Allowing the user to perform actions other than entering
         # utterances
         if len(intent.utterances) >= 1:
             if command == "q":
-                promptMode = "slot"
+                self.promptMode = "chooseUtterance"
             elif command == "undo":
                 del(intent.utterances[-1])
 
@@ -79,14 +95,43 @@ class Interface():
         else:
             intent.addUtterance(command)
             self.output = "'{}'\n".format(command) + "Looks good."
-            self.prompt = \
-            "Enter another utterance or enter 'q' to start creating slots:\n"
+
+    # Allows the user to choose which utternacne they wish to modify
+    def chooseUtterance(self, command):
+        # Choosing the correct utterance to modify if the user has, in fact,
+        # picked an utterance to modify
+        if command != "q":
+            try:
+                self.currUtteranceIndex = int(command)
+                if self.currUtteranceIndex > len(self.intent.utterances):
+                    self.output = "'" + command + "' is not a valid index number."
+                else:
+                    self.promptMode = "slot"
+            except:
+                self.output = "'" + command + "' is not a valid index number."
+        else:
+            self.output = "Select one of the following utterances to add slots to it. If you don't wish to add slots to any, enter 'q'.\n"
+            for i in range(0, len(self.intent.utterances), 1):
+                self.output += i + ". " + self.intent.utterances[i] + "\n"
 
     # Creates an intuitive prompt/interface for the user to add slots to
     # specific utternaces
-    def processUtterance(self, command):
-        self.intent.currUtterance.grabSlots()
-        
+    def prepCurrUtterance(self):
+        slots = self.intent.utterances[self.currUtteranceIndex].grabSlots()
+        self.output = "Selected utterance: {}"\
+            .format(self.utterances[self.currUtteranceIndex])
+        self.prompt = "Your current slots are " + slots + \
+                "If you wish to add more, enter the word you want to replace. (Enter 'q' if don't want to add a slot.): "
+
+    def addSlots(self, command):
+        if command == "q":
+            #TODO: Change to next mode (I think it's just slot type and done)
+            print("Work left to do")
+        elif command == "change utterance":
+            self.promptMode = "chooseUtterance"
+        else:
+        phrase = self.intent.utterances[self.currUtteranceIndex]
+        if 
 
     def runUpdateIntent(self):
         pass
