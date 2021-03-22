896d6fbf49edd7282d918f113a928eada15a99c2
Michael Clinesmith <clinmic2@bvu.edu>
2021-03-22 18:29:22 -0500

    Just pushing this to have it backed up. Code isn't used anywhere yet

--- init/initScript.py
+++ init/initScript.py
@@ -3,21 +3,13 @@ import subprocess
 from pathlib import *
 
 class Initializer():
-    def __init__(self, name):
+    def __init__(self, name="new_proj"):
         self.proj_name = name
-
-
-    def __cloneSDK(self, projPath, sdkPath):
-        for child in sdkPath.iterdir():
-            currPath = projPath / child.name
-            currPath.symlink_to(child)
 
 
     def run(self):
         os.mkdir(self.proj_name)
         currPath = Path(self.proj_name)
-        sdkPath = Path("/home/michael/alexasrc")
-        self.__cloneSDK(currPath, sdkPath)
 
 
 if __name__ == "__main__":
